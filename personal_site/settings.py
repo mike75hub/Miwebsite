5ba980125ecd5fc48fe7139dd704326212ff0c8a
Mike Ouma <oumamikewab@gmail.com>
2025-12-13 23:23:26 +0300

    feat: Switch database from MySQL to SQLite for local development

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -77,14 +77,9 @@ WSGI_APPLICATION = 'personal_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD':'flashred',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3 ',
+        
     }
 }
 
